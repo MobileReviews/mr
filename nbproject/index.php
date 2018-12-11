9411950168b80c7de491db16f00fcd59071b78ab
Mantas <Mantas@DESKTOP-BQSH63F>
2018-12-11 15:27:55 +0200

    Hello world commit

--- nbproject/index.php
+++ nbproject/index.php
@@ -1,8 +1,9 @@
-<?php
-return "<p>Priviet pasauli</p>";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Home page</title>
+ </head>
+ <body>
+ <?php echo '<p>Priviet pasauli!</p>'; ?> 
+ </body>
+</html>
