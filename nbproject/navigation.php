caa60c5ad95e055e70a83a679fc5c602277b682b
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-17 15:35:10 +0200

    Logo

--- nbproject/navigation.php
+++ nbproject/navigation.php
@@ -1,8 +1,8 @@
 <?php
 return "
 <ul>
-    <li><a class='logo' href='welcome.php'><img src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png' 
-    alt='logo' height='32' width='32'></a></li>
+    <a class='logo' href='welcome.php'><img src='https://i.imgur.com/l2uTzOA.png' 
+    alt='logo' height='32' width='64'></a>
     <li><a class='navlink' href='logout.php'>Logout</a></li>     
     <li><a class='navlink' href='reset-password.php'>Reset Password</a></li>
     <li><a class='navlink' href='welcome.php'>Home</a></li>
