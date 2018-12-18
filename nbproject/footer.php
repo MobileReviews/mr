4ea03fa7cc93ef290637d26275ac515f431a3359
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-18 17:01:27 +0200

    Footer.

---
+++ nbproject/footer.php
@@ -0,0 +1,31 @@
+<?php
+return "
+<footer>
+    <div class='row'>
+        <div class='col-3 first-col'>
+           <img src='https://i.imgur.com/YGoorZz.png' alt='logo' height='128' width='256'>
+        </div>
+        <div class='col-3 contacts second-col'>
+              <i class='fas fa-user-circle'></i><p class='info'>Deividas Kacas</p> <br>
+              <i class='fas fa-envelope'></i><p class='info'>deividas.kacas@gmail.com</p> <br>
+              <i class='fas fa-building'></i><p class='info'>Kaunas, Lithuania</p> <br>
+              <a href='https://www.linkedin.com/in/deividas-kacas-b85875173/'><i class='fab fa-linkedin'></i></a> 
+              <a href='https://www.facebook.com/deivis.kaa?ref=br_rs'><i class='fab fa-facebook'></i></a> 
+              <a href='https://twitter.com/dei_wee'><i class='fab fa-twitter'></i></a> 
+              <a href='https://github.com/SneakyAF'><i class='fab fa-github'></i></a>    
+        </div>
+        <div class='col-3 contacts third-col'>
+              <i class='fas fa-user-circle'></i><p class='info'>Mantas Antanaitis</p> <br>
+              <i class='fas fa-envelope'></i><p class='info'>man.antanaitis@gmail.com</p> <br>
+              <i class='fas fa-building'></i><p class='info'>Vilnius, Lithuania</p> <br>
+              <a href='https://www.linkedin.com/in/mantas-antanaitis-777048153/'><i class='fab fa-linkedin'></i></a> 
+              <a href='https://www.facebook.com/K.Kolumbas'><i class='fab fa-facebook'></i></a> 
+              <a href='https://twitter.com/Mantas08703816'><i class='fab fa-twitter'></i></a> 
+              <a href='https://github.com/mantaselk'><i class='fab fa-github'></i></a>
+        </div>
+        <div class='col-3 fourth-col'>
+               <p class='copy'>&copy; 2018. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+";
