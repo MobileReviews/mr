08093866eb62cfce46b16b41b14c078b0829e0a9
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-19 11:34:37 +0200

    Bigger Logo

--- nbproject/navigation.php
+++ nbproject/navigation.php
@@ -1,8 +1,8 @@
 <?php
 return "
 <ul>
-    <a class='logo' href='welcome.php'><img src='https://i.imgur.com/YGoorZz.png' 
-    alt='logo' height='32' width='64'></a>
+    <a class='logo' href='welcome.php'><img src='https://i.imgur.com/JjCj9JC.png' 
+    alt='logo' height='32' width='100'></a>
     <li><a class='navlink' href='logout.php'>Logout</a></li>     
     <li><a class='navlink' href='reset-password.php'>Reset Password</a></li>
     <li><a class='navlink' href='welcome.php'>Home</a></li>
