6869e683b572da87445f79b95a00f33b5e9bf4d2
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-19 12:46:14 +0200

    Bigger Logo

--- nbproject/navigation.php
+++ nbproject/navigation.php
@@ -5,6 +5,7 @@ return "
     alt='logo' height='32' width='100'></a>
     <li><a class='navlink' href='logout.php'>Logout</a></li>     
     <li><a class='navlink' href='reset-password.php'>Reset Password</a></li>
+    <li><a class='navlink' href='uploads.php'>Admin Panel</a></li>
     <li><a class='navlink' href='welcome.php'>Home</a></li>
 </ul>
 ";
