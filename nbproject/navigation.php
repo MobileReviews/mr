381a06a95af4c161324e6a307c3348cba87d97f1
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-19 16:48:58 +0200

    new navigation

--- nbproject/navigation.php
+++ nbproject/navigation.php
@@ -1,11 +1,19 @@
 <?php
-return "
-<ul>
-    <a class='logo' href='welcome.php'><img src='https://i.imgur.com/JjCj9JC.png' 
-    alt='logo' height='32' width='100'></a>
-    <li><a class='navlink' href='logout.php'>Logout</a></li>     
-    <li><a class='navlink' href='reset-password.php'>Reset Password</a></li>
-    <li><a class='navlink' href='uploadbrand.php'>Admin Panel</a></li>
-    <li><a class='navlink' href='welcome.php'>Home</a></li>
-</ul>
-";
+    // Check if the user is logged in, if not then redirect him to login page
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: login.php");
+        exit;
+    }
+
+    echo "<ul>";
+    echo "<a class='logo' href='welcome.php'><img src='https://i.imgur.com/JjCj9JC.png'
+          alt='logo' height='32' width='100'></a>";
+    echo "<li><a class='navlink' href='logout.php'>Logout</a></li>";
+    echo "<li><a class='navlink' href='reset-password.php'>Reset Password</a></li>";
+    if($_SESSION["admin"] == "true"){
+        echo "<li><a class='navlink' href='uploadbrand.php'>Admin Panel</a></li>";
+    }
+    echo "<li><a class='navlink' href='welcome.php'>Home</a></li>";
+    echo "</ul>";
+?>
+
