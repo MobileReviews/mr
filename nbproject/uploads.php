486805049f2701e193f7ebcae5c6e2fd26f330e8
Mantas <Mantas@DESKTOP-BQSH63F.mshome.net>
2018-12-19 13:54:00 +0200

    Fixes to remove bootstrap

--- nbproject/uploads.php
+++ nbproject/uploads.php
@@ -15,7 +15,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
