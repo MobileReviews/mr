f1464c0af1f14bb9b23bad9ee621fbdf17c27a56
Deivis <Deivis@DESKTOP-MK7482Q>
2018-12-19 13:58:31 +0200

    new

---
+++ nbproject/imagevalidation.php
@@ -0,0 +1,30 @@
+<?php
+
+function checkImage($image, $directory)
+{
+    $filePath = $directory . basename($_FILES["fileToUpload"]["name"]);
+    $imageFileType = strtolower(pathinfo($filePath,PATHINFO_EXTENSION));
+    if(getimagesize($image["tmp_name"]) !== false) {
+        if (file_exists($filePath)) {
+         $_SESSION['uploadError'] = "Image with such name already exists\r\n";
+         return false;
+        }
+        if ($image["size"] > 500000) {
+            $_SESSION['uploadError'] = "Image is too large to upload";
+            return false;
+        }
+        if($imageFileType != "jpg" &&
+           $imageFileType != "png" &&
+           $imageFileType != "jpeg" ) {
+           $_SESSION['uploadError'] = "Only JPG, PNG and JPEG is valid";
+           return false;
+        }
+        return true;   
+    } 
+    else {
+        $_SESSION['uploadError'] = "File is not an image.\r\n";
+        return false;
+    }
+}
+?>
+
---
+++ nbproject/uploadbrand.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body> 
+<?php
+  $navigation = include_once "navigation.php";
+  echo $navigation;
+require_once "config.php";
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true
+            || $_SESSION["admin"] !== "true"){
+    header("location: welcome.php");
+    exit;
+}
+
+
+  
+include 'imagevalidation.php';
+if(isset($_POST["submit"])) {
+    $directory = "brand_img/";
+    $filePath = $directory . basename($_FILES["fileToUpload"]["name"]);
+    if (!checkImage($_FILES["fileToUpload"],$directory)) {
+        echo $_SESSION['uploadError'];
+    } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filePath)) {
+        $name = $_POST["brand"];
+        $sql = "INSERT INTO brands (brand_name, image) VALUES ('$name', '$filePath')";
+            if(mysqli_query($link, $sql)){
+                header("Location: welcome.php");
+            }
+            else die("Error uploading brand: ". mysqli_error($link));
+    } else {
+        $_SESSION['uploadError'] = "Error uploading brand picture";
+        echo $_SESSION;
+    }
+    }
+}
+  $footer = include_once "footer.php";
+  echo $footer;
+?>   
+<form action="" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload" required="required">
+    <input type="text" name="brand" id="brandName" required="required" placeholder="Name of a brand">
+    <input type="submit" value="Upload brand" name="submit">
+</form>
+</body>
+</html>
